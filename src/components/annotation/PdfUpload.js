1b13c8c2842b949478412c2e585ae11553202cb0
Dominik Paľo <dominik.palo@student.tuke.sk>
2023-10-30 18:02:03 +0100

    repairing and creating design for features import and history of files change

--- src/components/annotation/PdfUpload.js
+++ src/components/annotation/PdfUpload.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import PdfTextDisplay from "./PdfTextDisplay";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFilePdf,
+  faFileWord,
+  faFileAlt,
+} from "@fortawesome/free-solid-svg-icons";
 
 const PdfUpload = ({ onUploadSuccess }) => {
   const [pdfTexts, setPdfTexts] = useState([]);
@@ -59,6 +65,21 @@ const PdfUpload = ({ onUploadSuccess }) => {
   const [isUploading, setIsUploading] = useState(false);
   const fileInputRef = useRef(null);
 
+  const getFileIcon = (filename) => {
+    const fileExtension = filename.split(".").pop();
+
+    switch (fileExtension) {
+      case "pdf":
+        return faFilePdf;
+      case "docx":
+        return faFileWord;
+      case "txt":
+        return faFileAlt;
+      default:
+        return faFileAlt; // Výchozí ikona pro ostatní typy souborů
+    }
+  };
+
   const cancelUpload = (event) => {
     event.stopPropagation(); // Stops the click event from reaching the parent div
     setSelectedFile(null);
@@ -73,85 +94,96 @@ const PdfUpload = ({ onUploadSuccess }) => {
     setIsUploading(false);
   };
   return (
-    <div className="p-10">
-      <h1 className="text-2xl font-bold mb-4">PDF Texts</h1>
-      <div className="mt-5 border-dashed border-4 border-gray-400 rounded relative">
-        <input
-          type="file"
-          accept=".pdf"
-          className="absolute inset-0 w-full h-full opacity-0 cursor-pointer z-0"
-          onChange={(e) => setSelectedFile(e.target.files[0])}
-          ref={fileInputRef}
-        />
-        {selectedFile ? (
-          <div className="flex items-center space-x-2 p-5">
-            <span>{selectedFile.name}</span>
-            <button
-              onClick={cancelUpload}
-              className="text-red-500 z-10 bg-white rounded-full p-1"
-            >
-              x
-            </button>
+    <div className="p-10 m-auto text-white">
+      <div className="flex flex-col">
+        <div className="flex flex-row h-[300px] justify-center gap-32">
+          <div className="mt-5 border-dashed border-4 w-1/3  border-white  relative">
+            <input
+              type="file"
+              accept=".pdf"
+              className="absolute inset-0 w-full h-full opacity-0 cursor-pointer z-0"
+              onChange={(e) => setSelectedFile(e.target.files[0])}
+              ref={fileInputRef}
+            />
+            {selectedFile ? (
+              <div className="flex items-center justify-center">
+                <span>{selectedFile.name}</span>
+                <button
+                  onClick={cancelUpload}
+                  className="text-red-500 z-10 bg-white rounded-full p-1"
+                >
+                  x
+                </button>
+              </div>
+            ) : (
+              <div className="flex flex-col items-center self-center w-full justify-center py-10 z-10">
+                <p className="mb-2">Drag & Drop file here</p>
+                <p className="mb-2">OR</p>
+                <button className="px-4 py-2 bg-blue-500 text-white rounded">
+                  Browse Files
+                </button>
+              </div>
+            )}
           </div>
-        ) : (
-          <div className="flex flex-col items-center justify-center py-10 z-10">
-            <p className="mb-2">Drag & Drop file here</p>
-            <p className="mb-2">OR</p>
-            <button className="px-4 py-2 bg-blue-500 text-white rounded">
-              Browse Files
-            </button>
-          </div>
-        )}
-      </div>
-      <button
-        onClick={enhancedUploadPdf}
-        disabled={!selectedFile || isUploading}
-        className="mt-5 px-6 py-2 bg-green-500 text-white rounded shadow-lg hover:bg-green-600"
-      >
-        {isUploading ? "Uploading..." : "Upload"}
-      </button>
 
-      <div className="mt-5">
-        {pdfTexts.length > 0 && (
-          <div>
-            <table className="min-w-full bg-white">
-              <thead>
-                <tr>
-                  <th className="w-1/3  border-b border-gray-200 bg-gray-50 text-left px-4 py-2 text-gray-600 uppercase tracking-wider">
-                    Icon
-                  </th>
-                  <th className="w-1/3  border-b border-gray-200 bg-gray-50 text-left px-4 py-2 text-gray-600 uppercase tracking-wider">
-                    Filename {/* Zmenili sme názov stĺpca */}
-                  </th>
-                  <th className="w-1/3  border-b border-gray-200 bg-gray-50 text-left px-4 py-2 text-gray-600 uppercase tracking-wider">
-                    Actions
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {pdfTexts.map((text) => (
-                  <tr key={text.id} className="text-gray-700">
-                    <td className="w-1/3 text-left py-3 px-4">
-                      <i className="far fa-file-pdf text-xl"></i>
-                    </td>
-                    <td className="w-1/3 text-left py-3 px-4">
-                      {text.filename}
-                    </td>{" "}
-                    {/* Zmenili sme z text.id na text.filename */}
-                    <td className="w-1/3 text-left py-3 px-4">
-                      <button
-                        className="px-4 py-2 bg-red-500 text-white rounded"
-                        onClick={() => deletePdfText(text.id)}
-                      >
-                        Delete
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+          <div className="mt-5 flex-2 font-base relative group">
+            {pdfTexts.length > 0 && (
+              <div className="h-[calc(7*2.5rem)] z-10 relative overflow-y-auto">
+                {" "}
+                {/* Pevná výška pro 3 soubory a povolený vertikální posun */}
+                <table className="min-w-full border-2 bg-black text-white z-10">
+                  <thead>
+                    <tr>
+                      <th className="w-1/3 z-10   border-b border-gray-200 bg-gray-50   py-2 text-gray-600 uppercase tracking-wider">
+                        Icon
+                      </th>
+                      <th className="w-1/3 z-10 border-b border-gray-200 bg-gray-50 text-left px-4 py-2 text-gray-600 uppercase tracking-wider">
+                        Filename 
+                      </th>
+                      <th className="w-1/3 z-10 border-b border-gray-200 bg-gray-50 text-left px-4 py-2 text-gray-600 uppercase tracking-wider">
+                        Actions
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {pdfTexts.map((text) => (
+                      <tr key={text.id} className="text-white">
+                        <td className="w-1/3 mt-2  m-auto flex py-3">
+                          <FontAwesomeIcon
+                            icon={faFilePdf}
+                            className="text-3xl"
+                          />
+                        </td>
+                        <td className="w-1/3 text-left py-3 px-4">
+                          {text.filename}
+                        </td>{" "}
+                        {/* Zmenili sme z text.id na text.filename */}
+                        <td className="w-1/3 text-left py-3 px-4">
+                          <button
+                            className="px-4 py-2 bg-red-500 text-white rounded"
+                            onClick={() => deletePdfText(text.id)}
+                          >
+                            Delete
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            )}
+            <div className="absolute top-[10px] left-[30px] w-[95%] h-[100%] bg-[#1AFF15] lg:block hidden transition-colors"></div>
           </div>
-        )}
+        </div>
+        <div className="flex">
+          <button
+            onClick={enhancedUploadPdf}
+            disabled={!selectedFile || isUploading}
+            className="mt-5 px-6 py-2 bg-green-500 text-white rounded shadow-lg hover:bg-green-600"
+          >
+            {isUploading ? "Uploading..." : "Upload"}
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/pages/FeaturesPage.js
+++ src/pages/FeaturesPage.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Header from '../components/layout/Header';
-import Footer from '../components/layout/Footer';
-import AnnotationForm from '../components/annotation/AnnotationForm';
-import PdfUpload from '../components/annotation/PdfUpload';
-import FileUploader from '../components/annotation/FileUploader';
-import PdfTextDisplay from '../components/annotation/PdfTextDisplay';
+import Header from "../components/layout/Header";
+import Footer from "../components/layout/Footer";
+import AnnotationForm from "../components/annotation/AnnotationForm";
+import PdfUpload from "../components/annotation/PdfUpload";
+import FileUploader from "../components/annotation/FileUploader";
+import PdfTextDisplay from "../components/annotation/PdfTextDisplay";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
 function FeaturesPage() {
   const [pdfTexts, setPdfTexts] = useState([]);
- 
 
   const fetchPdfTexts = async () => {
     try {
@@ -33,16 +35,42 @@ function FeaturesPage() {
   useEffect(() => {
     fetchPdfTexts();
   }, []);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
   return (
-    <div className='w-full h-full'>
-      <Header/>
-      <h1>Welcome to the Features Page</h1>
-      <AnnotationForm/>
-      <PdfUpload onUploadSuccess={handleUploadSuccess} />
-      <FileUploader/>
-      <PdfTextDisplay pdfTexts={pdfTexts} onDelete={deletePdfText} />
-      <Footer/>
-    </div>
+    <section className="bg-gray-200 font-base text-white">
+      <Header />
+      {/* Heading */}
+      <div className="relative group p-1 mt-10 mx-auto w-full sm:w-2/3 md:w-1/2 lg:w-1/3">
+        <h1 className="relative z-10 bg-white text-4xl font-bold mb-16 p-2 text-black flex items-center justify-center py-4 px-12 uppercase border-black border-2">
+          FEATURES PAGE
+        </h1>
+        <div className="absolute top-[15px] left-[12px] w-[98%] h-[50%] bg-[#F700C6] lg:block hidden transition-colors"></div>
+      </div>
+      {/* Search Bar */}
+      <div className="relative group p-1 mx-auto w-full sm:w-2/3 md:w-1/2 lg:w-1/3 ">
+        <div className="relative z-10 flex w-full items-center bg-white border-2 border-black">
+         <FontAwesomeIcon className="ml-5 text-black" icon={faSearch}/>
+          <input
+            type="text"
+            placeholder="Search through your imports or annotations"
+            className="flex-grow p-2 placeholder-gray-500 outline-none"
+          />
+        </div>
+        <div className="absolute top-[10px] left-[12px] w-[98%] h-[90%] bg-[#F700C6] lg:block hidden transition-colors"></div>
+      </div>
+    
+      {/* <AnnotationForm/> */}
+      <div>
+        <PdfUpload onUploadSuccess={handleUploadSuccess} />
+
+        <PdfTextDisplay pdfTexts={pdfTexts} onDelete={deletePdfText} />
+      </div>
+      <Footer />
+    </section>
   );
 }
 
